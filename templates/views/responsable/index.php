104413a36f34d46f836146add8eccaac56e96695
Maxence <maxence.gvr@gmail.com>
2023-09-27 11:28:51 +0200

    added calendar input

--- templates/views/responsable/index.php
+++ templates/views/responsable/index.php
@@ -20,16 +20,31 @@
                         <div class="container">
                             <div class="container_head">
                                 <span>Bonjour,</span>
-                                <h1><?php echo $this->user['nom_resp_stage'] . " " . $this->user['prenom_resp_stage'] ?></h1>
+                                <h1><?php echo "{$this->user['nom_resp_stage']} {$this->user['prenom_resp_stage']}" ?></h1>
                             </div>
                             <div class="container_body">
+                                <div class="general_container">
+                                    <!-- IMPR PDF -->
+                                    <!-- DECONEXION -->
+                                </div>
                                 <div class="params_container">
                                     <span class="params_title"><b>Paramètres d'inscription</b></span>
-                                    <div>
-                                        <?php echo $this->user['date_deb_insc'] ?>
-                                        <?php echo $this->user['date_fin_insc'] ?>
-                                        <?php echo $this->user['nb_max_entretiens'] ?>
-                                    </div>
+                                    <form action="/save" method="POST">
+                                        <span class="params_subtitle">Période d'inscription</span>
+                                        <div class="flex_container column">
+                                            <input required type="date" id="dateInscStart" name="dateStart" value="<?php echo $this->user["date_deb_insc"] ?>" hidden>
+                                            <input required type="date" id="dateInscEnd" name="dateEnd" value="<?php echo $this->user["date_fin_insc"] ?>" hidden>
+                                            <?php $this->component(Components\Calendar::class) ?>
+                                        </div>
+                                        <span class="params_subtitle">Nombre max. d'entretiens</span>
+                                        <div class="flex_container">
+                                            <input type="number">
+                                        </div>
+                                        <div class="flex_container space">
+                                            <button class="btn validate" type="submit">Valider</button>
+                                            <button class="btn cancel" type="reset">Restaurer</button>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
