4a84f8cb93e218776bd9830767868819fcd4f08d
Maxence <maxence.gvr@gmail.com>
2023-09-27 11:27:05 +0200

    merge conflicts

--- src/controllers/Index.php
+++ src/controllers/Index.php
@@ -1,33 +1,43 @@
 <?php
-    namespace App\Controllers;
 
-    use Service\Routes\Response;
-    use Service\Interfaces\Controller;
+namespace App\Controllers;
 
-    use Templates\Views;
+use Service\Routes\Response;
+use Service\Interfaces\Controller;
 
-    class Index extends Controller {
-        public function index (array $user = []) : Response
-        {
-            if(empty($user)) {
-                $user = (new LoginController())->auth();
-            }
+use Templates\Views;
 
-            if($user["isResp"]) {
-                return $this->responsable($user);
-            } else {
-                return $this->etudiant($user);
-            }
+class Index extends Controller
+{
+    public function index(array $user = []): Response
+    {
+        if (empty($user)) {
+            $user = (new LoginController())->auth();
         }
 
-        private function responsable (array $user) : Response {
-            return Response::template(Views\Responsable\Index::class, [
-                "user" => $user
-            ], 200);
-        }
-
-        private function etudiant (array $user) : Response {
-            return new Response("etudiant", 200);
+        if ($user["isResp"]) {
+            return $this->responsable($user);
+        } else {
+            return $this->etudiant($user);
         }
     }
-?>
+
+    private function responsable (array $user) : Response {
+        return Response::template(Views\Responsable\Index::class, [
+            "user" => $user,
+            "title" => "Responsable Stage {$user['nom_BUT']} {$user['annee_BUT']}"
+        ], 200);
+    }
+    private function etudiant(array $user): Response
+    {
+        return Response::template(Views\Etudiants\listeEntreprise::class, [
+            "user" => $user,
+            "title" => "Etudiant {$user['prenom_etudiant']} {$user['nom_etudiant']}",
+            "date_debut" => '09-12-2023',
+            "date_fin" => '18-12-2023',
+            "nom_entreprise" => 'Schneider',
+            "dpt_entreprise" => 'Charente',
+            "ville_entreprise" => 'Angoulême',
+        ], 200);
+    }
+}
--- src/controllers/index.php
+++ src/controllers/index.php
@@ -22,17 +22,17 @@ class Index extends Controller
         }
     }
 
-    private function responsable(array $user): Response
-    {
+    private function responsable (array $user) : Response {
         return Response::template(Views\Responsable\Index::class, [
-            "user" => $user
+            "user" => $user,
+            "title" => "Responsable Stage {$user['nom_BUT']} {$user['annee_BUT']}"
         ], 200);
     }
-
     private function etudiant(array $user): Response
     {
         return Response::template(Views\Etudiants\listeEntreprise::class, [
             "user" => $user,
+            "title" => "Etudiant {$user['prenom_etudiant']} {$user['nom_etudiant']}",
             "date_debut" => '09-12-2023',
             "date_fin" => '18-12-2023',
             "nom_entreprise" => 'Schneider',
