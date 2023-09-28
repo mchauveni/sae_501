1043d5d5270a9c0328fbb990131a28d9e758666d
Maxence <maxence.gvr@gmail.com>
2023-09-28 19:09:53 +0200

    Update Formation.php

--- vendor/database/entities/Formation.php
+++ vendor/database/entities/Formation.php
@@ -16,7 +16,8 @@
             WHERE
                 e.id_formation = :id_formation
             GROUP BY
-                e.id_etudiant, e.nom_etudiant, e.prenom_etudiant, e.id_formation, e.tel_etudiant, e.email_etudiant";
+                e.id_etudiant, e.nom_etudiant, e.prenom_etudiant, e.id_formation, e.tel_etudiant, e.email_etudiant
+            ORDER BY nb_entretiens DESC";
             return PDO::query($sql, [":id_formation" => $id])->fetchAll();
         }
     }
