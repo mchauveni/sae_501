b8aecdeadda760b5266b6b45a9157b84ede7b954
mchauveni <matheo.chaumet.venier@gmail.com>
2023-09-28 18:12:18 +0200

    fixed css refactor

--- templates/views/responsable/listeEtudiants.php
+++ templates/views/responsable/listeEtudiants.php
@@ -21,33 +21,38 @@ class listeEtudiants extends Template {
             <main>
                 <div class="container">
                     <section class="return">
-                        <a href="/">
-                            < Retour </a>
+                        <?php $this->component(Components\button::class, [
+                            "content" => "retour",
+                            "color" => "dark",
+                            "icon" => "chevron-reverse",
+                            "iconBeforeText" => true,
+                            "target" => "/"
+                        ]) ?>
                     </section>
                     <div class="user">
                         <span class="user__greeting">Bonjour,</span>
-                        <h1><?php echo "{$this->user['nom_resp_stage']} {$this->user['prenom_resp_stage']}" ?></h1>
+                        <h1 class="user__name"><?php echo "{$this->user['nom_resp_stage']} {$this->user['prenom_resp_stage']}" ?></h1>
                         <p class="user__subtitle">Voici la liste des étudiants de la formation</p>
                     </div>
                     <div class="list_container">
-                        <?php foreach($this->etudiants as $etudiant) {
-                            ?>
-                                <div class="list_item">
-                                    <h4><?php echo "{$etudiant['nom_etudiant']} {$etudiant['prenom_etudiant']}" ?></h4>
-                                    <p><?php echo $etudiant['email_etudiant'] ?></p>
-                                    <p><?php echo $etudiant['tel_etudiant'] ?></p>
-                                    <h3><?php echo $etudiant['nb_entretiens'] ?></h3>
-                                </div>
-                            <?php
+                        <?php foreach ($this->etudiants as $etudiant) {
+                        ?>
+                            <div class="list_item">
+                                <h4><?php echo "{$etudiant['nom_etudiant']} {$etudiant['prenom_etudiant']}" ?></h4>
+                                <p><?php echo $etudiant['email_etudiant'] ?></p>
+                                <p><?php echo $etudiant['tel_etudiant'] ?></p>
+                                <h3><?php echo $etudiant['nb_entretiens'] ?></h3>
+                            </div>
+                        <?php
                         } ?>
                     </div>
                     <div class="general_container">
-                    <?php $this->component(Components\button::class, [
-                                "content" => "Imprimer en PDF",
-                                "icon" => "open_in_new",
-                                "targetBlank" => true,
-                                "target" => "/liste-etudiants?pdf"
-                            ]); ?>
+                        <?php $this->component(Components\button::class, [
+                            "content" => "Imprimer en PDF",
+                            "icon" => "open_in_new",
+                            "targetBlank" => true,
+                            "target" => "/liste-etudiants?pdf"
+                        ]); ?>
                     </div>
                 </div>
             </main>
